7535df3b25ada5d07e2df48b20d86a60ef8f27b6
Larakh88 <lara.khoury@hotmail.com>
2023-02-21 08:01:48 +0400

    fixed private functions

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: lel-khou <lel-khou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 16:03:54 by lel-khou          #+#    #+#             */
-/*   Updated: 2023/02/20 22:16:08 by lel-khou         ###   ########.fr       */
+/*   Updated: 2023/02/21 08:00:26 by lel-khou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ class PhoneBook {
 private:
 	Contact Contacts[8];
 	static int	size;
+	
+	static int	getSize(void);
 
 public:
 	PhoneBook(void);
@@ -29,7 +31,6 @@ public:
 	void		setContact(std::string info[5], int i);
 	void		printContact(int i);
 	void		printIndexTable(int i);
-	static int	getSize(void);
 };
 
 #endif
